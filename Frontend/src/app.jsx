b89d448fb38f4b05a390d7ded85c0f2d6db94a68
Atilla1907 <atilla.gorgulu@chasacademy.se>
2025-05-21 12:27:19 +0200

    feat: added simple routs for now

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Hello from Vite + React</h1>} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
   );
 }
 
